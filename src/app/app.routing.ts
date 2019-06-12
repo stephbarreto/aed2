a4648f31481a5f011c8835425287bc9c6cdbbea6
Steph <sdsb.snf@uea.edu.br>
2019-06-12 17:14:33 -0400

    first commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent }   from './dashboard/dashboard.component';
+
+export const AppRoutes: Routes = [
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    }
+]
